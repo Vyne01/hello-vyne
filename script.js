703c89f1aec0fb49691720fc80f55ee32d441688
Viza Alluri Rohit Reddy <vizaallurirohit@gmail.com>
2025-07-11 10:30:05 +0530

    moved index.html to root for Vercel

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
 function joinWaitlist() {
   alert("Coming soon! Vyne is almost ready to meet you.");
 }
-function scrollToAbout() {
-  document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
-}
